f5ab5e0c8996646fe0cb5c85f199ad5b3c29fd58
ilyas <73260385+Mr-TalhaIlyas@users.noreply.github.com>
2021-04-13 15:28:03 +0900

    fixed

--- scripts/CRF_class.py
+++ scripts/CRF_class.py
@@ -4,83 +4,56 @@ import matplotlib.pyplot as plt
 import pydensecrf.densecrf as dcrf
 from pydensecrf.utils import compute_unary, unary_from_softmax
 import tensorflow as tf
+from gray2color import gray2color
 import imageio
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 300
 '''
 This script implements CRF as described in Deeplab paper it takes about 0.2 seconds per image.
 '''
-n_classes = 30
-pallet =  np.array([[[ 0,  0,  0],
-                    [ 0,  0,  0],
-                    [ 0,  0,  0],
-                    [ 0,  0,  0],
-                    [ 0,  0,  0],
-                    [111, 74,  0],
-                    [ 81,  0, 81],
-                    [128, 64,128],
-                    [244, 35,232],
-                    [250,170,160],
-                    [230,150,1400],
-                    [ 70, 70, 70],
-                    [102,102,156],
-                    [190,153,153],
-                    [180,165,180],
-                    [150,100,100],
-                    [150,120, 90],
-                    [153,153,153],
-                    [153,153,153],
-                    [250,170, 30],
-                    [220,220,  0],
-                    [107,142, 35],
-                    [152,251,152],
-                    [70,130,180],
-                    [220, 20, 60],
-                    [255,  0,  0],
-                    [ 0,  0,142],
-                    [ 0,  0, 70],
-                    [ 0, 60,100],
-                    [ 0,  0, 90],
-                    [0,  0,110],
-                    [ 0, 80,100],
-                    [ 0,  0,230],
-                    [119, 11, 32]]], np.uint8) / 255
 class Seg_CRF:
-    '''
-
-    Parameters
-    ----------
-    img_path : path to an image, 
-                    Format [H, W, 3]; values ranging from [0, 255]
-    model_op_path : path model output of the same input image.
-                    Format [H, W]; values ranging from [0, num_of_classes]
-    num_of_classes : number of classes in a dataset e.g. in cityscape has 30 classes
-    clr_op : color the output or not a bool
-    pallet: a [1 x no of classes x 3] array containing the RGB values of classes, type float32
-    
-    Returns
-    -------
-    crf op image as array range [0, 1] grayscael or RGB
-    
-    '''
-    def __init__(self, img_path, model_op_path, num_of_classes, clr_op=False, pallet = None):
-        
+    def __init__(self, img_path, model_op_path, num_of_classes, img_w=1024, img_h=512,
+                 apperance_kernel=[8, 164, 100], spatial_kernel=[3, 10], clr_op=False, pallet2use ='ade20k'):
+        '''
+        Parameters
+        ----------
+        img_path : path to an image, 
+                        Format [H, W, 3]; values ranging from [0, 255]
+        model_op_path : path model output of the same input image.
+                        Format [H, W]; values ranging from [0, num_of_classes]
+        num_of_classes : number of classes in a dataset e.g. in cityscape has 30 classes
+        clr_op : color the output or not a bool
+        pallet2use : see https://pypi.org/project/gray2color/ for details
+        img_w : for resizing image and mask to same size default is 1024
+        img_h : for resizing image and mask to same size default is 512
+        apperance_kernel : The PairwiseBilateral term in CRF a list of values in order [sxy, srgb, compat]  
+                            default values are [8, 164, 100]
+        spatial_kernel : The PairwiseGaussian term in CRF a list of values in order [sxy, compat]  
+                            default values are [3, 10]
+        Returns
+        -------
+        crf op image as array range [0, 1] grayscael or RGB
+        '''
         self.img_path = img_path
         self.model_op_path = model_op_path
         self.num_of_classes = num_of_classes
-        self.img_w = 1024
-        self.img_h = 512
+        self.img_w = img_w
+        self.img_h = img_h
         self.n_classes = num_of_classes
-        self.pallet = pallet
+        self.pallet2use = pallet2use
         self.clr_op = clr_op
+        self.apperance_kernel = apperance_kernel
+        self.spatial_kernel = spatial_kernel
         # read iamges
-        self.image = cv2.imread(self.img_path) #/ 255    #D:/Anaconda/Image_analysis/cat.png
+        self.image = cv2.imread(self.img_path) 
         self.image = cv2.resize(self.image, (self.img_w, self.img_h))
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-        self.mask = cv2.imread(self.model_op_path, 0)# / 255
+        self.mask = cv2.imread(self.model_op_path, 0)
         self.mask = cv2.resize(self.mask, (self.img_w, self.img_h))
         
     def start(self):
+        if self.n_classes < 2:
+            self.n_classes = 2
         # covnert masks form [W, H] to [W, H , n_classes]
         if self.n_classes <= 2:
             inverse_mask = cv2.bitwise_not(self.mask)
@@ -108,10 +81,11 @@ class Seg_CRF:
         d.setUnaryEnergy(unary)# add uniray potential to paiwise potential
         # Pairwise potential
         # it smooths the masks  # 5     10 sxy=(.5, .5)
-        d.addPairwiseGaussian(sxy=(3), compat=10, kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC) # Spatial/Smootheness Kernel
-        d.addPairwiseBilateral(sxy=(8), srgb=(164), rgbim=self.image, #5  13 10  sxy=(1.5, 1.5), srgb=(64, 64, 64)
-                               compat=100,
-                               kernel=dcrf.DIAG_KERNEL,
+        d.addPairwiseGaussian(sxy=self.spatial_kernel[0], compat=self.spatial_kernel[1],
+                              kernel=dcrf.DIAG_KERNEL, normalization=dcrf.NORMALIZE_SYMMETRIC) # Spatial/Smootheness Kernel
+        
+        d.addPairwiseBilateral(sxy=self.apperance_kernel[0], srgb=self.apperance_kernel[1], rgbim=self.image, #5  13 10  sxy=(1.5, 1.5), srgb=(64, 64, 64)
+                               compat=self.apperance_kernel[2], kernel=dcrf.DIAG_KERNEL,
                                normalization=dcrf.NORMALIZE_SYMMETRIC)  # Appearance/approximity Kernel
         '''
         1. increasing sxy puts more focus on changing intensity
@@ -120,36 +94,34 @@ class Seg_CRF:
         '''
         Q = d.inference(1)
         CRF_op = np.argmax(Q, axis=0).reshape((self.image.shape[0], self.image.shape[1])).astype(np.float32) # Becarefull regardnig datatype
+       
         if self.clr_op:
-           CRF_op = self.gray2rgb(CRF_op, self.pallet)
-        return CRF_op
-    
-    def gray2rgb(self, gray_processed, pallet):
-
-        gray = gray_processed
-        w, h = gray.shape
-        gray = gray[:,:,np.newaxis]
-        gray = tf.image.grayscale_to_rgb((tf.convert_to_tensor(gray)))
-        sess = tf.compat.v1.Session()
-        gray = sess.run(gray)
-        gray = tf.cast(gray, 'int32')
-        sess1 = tf.compat.v1.Session()
-        gray = sess1.run(gray)
-        unq = np.unique(gray)
-        rgb = np.zeros((w,h,3))
-        
-        for i in range(len(unq)):
-            clr = pallet[:,unq[i],:]
-            rgb = np.where(gray!=unq[i], rgb, np.add(rgb,clr))
-        return rgb
+            CRF_op_rgb = gray2color(CRF_op, use_pallet=self.pallet2use)    
+            return CRF_op.astype(np.uint8), CRF_op_rgb
+        else:     
+            return CRF_op
 #%%        
-# Usage
-img_path='D:/Anaconda/Datasets/vistas/vistas/val/images/images/_1Gn_xkw7sa_i9GU4mkxxQ.jpg'
-model_op_path='D:/Anaconda/Datasets/vistas/vistas/val/masks/masks/_1Gn_xkw7sa_i9GU4mkxxQ.png'
 
-crf = Seg_CRF(img_path, model_op_path, 66, clr_op=True, pallet=pallet)
-CRF_op = crf.start()
-plt.imshow(CRF_op)
+# Zero pixels are consdered background
+# Usage
+img_path='D:/Anaconda/Image_analysis/cat.png'
+model_op_path='D:/Anaconda/Image_analysis/mask.png'
+
+# Default Values are
+apperance_kernel = [8, 164, 100] # PairwiseBilateral [sxy, srgb, compat]  
+spatial_kernel = [3, 10]         # PairwiseGaussian  [sxy, compat] 
+
+# or if you want to to specify seprately for each XY direction and RGB color channel then
+
+apperance_kernel = [(1.5, 1.5), (64, 64, 64), 100] # PairwiseBilateral [sxy, srgb, compat]  
+spatial_kernel = [(0.5, 0.5), 10]                  # PairwiseGaussian  [sxy, compat] 
+
+crf = Seg_CRF(img_path, model_op_path, 2, img_w=1024, img_h=512,
+                 apperance_kernel=apperance_kernel, spatial_kernel=spatial_kernel,
+                 clr_op=True, pallet2use ='cityscape')
+
+gray, rgb = crf.start()
+plt.imshow(rgb)
 
 
 
